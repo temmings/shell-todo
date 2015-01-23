2bbdf82fb2bc643374855f7aed7be1b0c0a1b119
Tetsuya HAMAMOTO <temmings@gmail.com>
2015-01-24 08:49:05 +0900

    Add unit test.

---
+++ test.prepare.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+curl -L "https://shunit2.googlecode.com/files/shunit2-2.1.6.tgz" | tar zx
---
+++ test.shell-todo.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+setUp() {
+    TODO_FILE=$(mktemp $SHUNIT_TMPDIR/$(basename $0).XXXXX)
+    TODO_FILE_BACKUP_EXT=backup_ext
+    source $(dirname $0)/shell-todo
+}
+
+#tearDown() {
+#}
+
+test_todo_swapfile() {
+    local file1=$(mktemp $SHUNIT_TMPDIR/$(basename $0).XXXXX)
+    local file2=$(mktemp $SHUNIT_TMPDIR/$(basename $0).XXXXX)
+    echo file 1 > $file1
+    echo file 2 > $file2
+    _todo_swapfile $file1 $file2 > /dev/null
+    assertEquals 'file 1' "$(cat $file2)"
+    assertEquals 'file 2' "$(cat $file1)"
+    rm $file1 $file2
+}
+
+test_todo_add() {
+    _todo_add 'test add todo' > /dev/null
+    assertEquals "test add todo" "$(tail -1 $TODO_FILE)"
+    _todo_add test unquoted todo > /dev/null
+    assertEquals "test unquoted todo" "$(tail -1 $TODO_FILE)"
+}
+
+test_todo_show() {
+    assertNull "$(_todo_show)"
+    _todo_add 'test show todo 1' > /dev/null
+    _todo_add 'test show todo 2' > /dev/null
+    _todo_add 'test show todo 3' > /dev/null
+    local result=$(_todo_show)
+    assertTrue '[[ "$result" =~ "test show todo 1" ]]'
+    assertTrue '[[ "$result" =~ "test show todo 2" ]]'
+    assertTrue '[[ "$result" =~ "test show todo 3" ]]'
+}
+
+test_todo_backup() {
+    _todo_add 'test backup todo' > /dev/null
+    _todo_backup > /dev/null
+    assertEquals "test backup todo" "$(tail -1 $TODO_FILE.$TODO_FILE_BACKUP_EXT)"
+    assertEquals "$(cat $TODO_FILE)" "$(cat $TODO_FILE.$TODO_FILE_BACKUP_EXT)"
+}
+
+test_todo_delete() {
+    _todo_add 'test delete todo' > /dev/null
+    _todo_add 'test delete todo 2' > /dev/null
+    _todo_delete 2 > /dev/null
+    assertEquals "test delete todo" "$(tail -1 $TODO_FILE)"
+    _todo_delete 1 > /dev/null
+    assertNull "$(tail -1 $TODO_FILE)"
+}
+
+test_todo_undo() {
+    _todo_add 'test undo todo' > /dev/null
+    _todo_add 'test redo todo' > /dev/null
+    _todo_undo > /dev/null
+    assertEquals "test undo todo" "$(tail -1 $TODO_FILE)"
+    _todo_undo > /dev/null
+    assertEquals "test redo todo" "$(tail -1 $TODO_FILE)"
+}
+
+source $(dirname $0)/shunit2-2.1.6/src/shunit2
